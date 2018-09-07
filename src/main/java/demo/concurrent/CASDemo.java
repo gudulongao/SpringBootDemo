345f19e7618216058c8087041f4235658e3f0944
gudulongao <gudulongao@qq.com>
2018-09-07 22:47:37 +0800

    增加CAS策略相关代码

--- src/main/java/demo/concurrent/CASDemo.java
+++ src/main/java/demo/concurrent/CASDemo.java
@@ -1,4 +1,35 @@
 package demo.concurrent;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * CAS策略样例
+ *
+ * @author tangleic
+ */
 public class CASDemo {
+    private AtomicInteger i = new AtomicInteger(0);
+
+    private void test() {
+        DemoRunner demoRunner = new DemoRunner();
+        Thread thread = null;
+        for (int num = 0; num < 1000; num++) {
+            thread = new Thread(demoRunner);
+            thread.start();
+        }
+        System.out.println(i.get());
+    }
+
+    class DemoRunner implements Runnable {
+        @Override
+        public void run() {
+            //自增
+            i.getAndIncrement();
+        }
+    }
+
+    public static void main(String[] args) {
+        CASDemo demo = new CASDemo();
+        demo.test();
+    }
 }
